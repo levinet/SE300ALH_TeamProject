ace46faffc51dbd25d6a38cb511be12a18a7b4bc
yessenia <y3ssgl0@gmail.com>
2015-11-13 15:13:35 -0500

    smsHTTP

--- src/EmergencyContact.java
+++ src/EmergencyContact.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -6,11 +9,15 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.net.*;
+
+
 
 /**
  * @author lopezy2, based on code from <a href="http://www.tutorialspoint.com/java/java_sending_email.htm">tutorialspoint.com</a>
  * @version 1.0
  * @created 09-Oct-2015 3:16:18 PM
+ * 
  */
 public class EmergencyContact{ //extends InfoGUI{
 	
@@ -64,8 +71,7 @@ public static void main (String [] args){
 	} catch (MessagingException  e) {
 		e.printStackTrace();
 	}
-}
-}
+
 
 
 // email validation \b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b.
@@ -74,8 +80,29 @@ public static void main (String [] args){
 
 // send SMS message
 
-
-
-
+	try {
+		String recipient = "+6825598090";
+		String message = " Greetings! Have a nice day!";
+		String username = "admin";
+		String password = "pasword";
+		String originator = "+6825598090";
+		String requestUrl  = "http://127.0.0.1:9501/api?action=sendmessage&" +
+		 "username=" + URLEncoder.encode(username, "UTF-8") +
+		 "&password=" + URLEncoder.encode(password, "UTF-8") +
+		 "&recipient=" + URLEncoder.encode(recipient, "UTF-8") +
+		 "&messagetype=SMS:TEXT" +
+		 "&messagedata=" + URLEncoder.encode(message, "UTF-8") +
+		 "&originator=" + URLEncoder.encode(originator, "UTF-8") +
+		 "&serviceprovider=GSMModem1" +
+		 "&responseformat=html";
+		URL url = new URL(requestUrl);
+		HttpURLConnection uc = (HttpURLConnection)url.openConnection();
+		System.out.println(uc.getResponseMessage());
+		uc.disconnect();
+		} catch(Exception ex) {
+		System.out.println(ex.getMessage());
+		}
+	}
+}
 
 //end EmergencyContact
